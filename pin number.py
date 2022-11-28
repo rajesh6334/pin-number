583deb9f4e9c39d04d97c404fd0d8681b04e2da2
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-11-28 21:05:37 +0530

    Add files via upload

---
+++ pin number.py
@@ -0,0 +1,10 @@
+#pin number valid or not
+pinnumber=6334
+print('enter the pin number')
+a=int (input())
+if a==pinnumber:
+    print('continue the transaction')
+else:
+        print('incorrect pin number')
+        print('plase try again')
+
